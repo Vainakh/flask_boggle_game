0d37446791f373099bbaac0c864f61fc426ca7de
Sarah <sjbrooks305@gmail.com>
2020-03-20 18:29:58 -0400

    added testing and form submission for button; no route for button submission yet

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-
 from flask import Flask, request, jsonify, render_template, redirect, session
-
-
 from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 from blinker import signal
 
 boggle_game = Boggle()
 
-
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret"
+# app.config['TESTING'] = True
 
 debug = DebugToolbarExtension(app)
 
@@ -21,4 +18,4 @@ def render_board():
     board = boggle_game.make_board()
     return render_template("board.html", board=board)
 
-    
+app.route('/check-word')
--- test.py
+++ test.py
@@ -7,4 +7,10 @@ from boggle import Boggle
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
+    def test_render_board(self):
+        with app.test_client() as client:
+            resp = client.get('/')
+            html = resp.get_data(as_text=True)
 
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<td>', html)
